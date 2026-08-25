01b4ac96e6ff0fea7ca99c42e997285f20b63e78
مهیار اسفندیاری <320553326+biotechmahyar@users.noreply.github.com>
2026-08-25 17:56:58 +0000

    Freebuff: Import { emailOtp } from "./auth/emailOtp"; ارور این هست

--- src/convex/auth.ts
+++ src/convex/auth.ts
@@ -3,7 +3,7 @@
 import { convexAuth } from "@convex-dev/auth/server";
 import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
 import { Password } from "@convex-dev/auth/providers/Password";
-import { emailOtp } from "./auth/emailOtp";
+import { emailOtp } from "./emailOtp";
 
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
