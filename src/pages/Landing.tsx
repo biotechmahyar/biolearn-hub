d8f7ca90534fb4e920329b7e1e810338cd5a3153
مهیار اسفندیاری <320553326+biotechmahyar@users.noreply.github.com>
2026-09-20 18:19:56 +0000

    Freebuff: Build Error: /home/daytona/codebase/src/pages/Landing.tsx...

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -142,7 +142,7 @@ export default function Landing() {
             </Link>
           </motion.div>
 
-          {/* Compute game */}
+          {/* Compute Game */}
           <motion.div
             initial={{ opacity: 0, y: 18 }}
             animate={{ opacity: 1, y: 0 }}
